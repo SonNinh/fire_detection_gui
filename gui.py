b1a25c165692453762ffad9314d49089fa6d7774
SonNinh <sonninhngocba@gmail.com>
2019-04-10 14:18:05 +0100

    2nd

--- gui.py
+++ gui.py
@@ -1,14 +1,21 @@
-#!/usr/bin/python3
 
-
+import numpy as np
 import cv2
 from tkinter import *
 from PIL import Image, ImageTk
 from datetime import datetime
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from time import sleep
+from threading import Thread
+import sys
 
 
+image = np.zeros((320, 240, 3), np.uint8)
 settingSpecs = {}
 resolution = [320, 240]
+end_thread = False
+
 def configButtonCllbck():
     global settingSpecs
 
@@ -27,6 +34,13 @@ def resButtonCllbck():
     else:
         resolution = [320, 240]
 
+
+def closeButtonCllbck():
+    global end_thread
+    end_thread = True
+    sleep(1)
+    root.destroy()
+    
 
 configButtonCllbck()
 
@@ -55,13 +69,13 @@ infoFrame2.pack(fill=BOTH, expand=True, side=RIGHT)
 f = open('config/gazSensorText', 'r')
 gazText = f.read()
 f.close()
-gazInfo = Text(infoFrame1, height=10, width=25, font=20, yscrollcommand=True)
+gazInfo = Text(infoFrame1, height=5, width=25, font=20, yscrollcommand=True)
 gazInfo.grid(row=0, column=0)
 
 f = open('config/tempSensorText', 'r')
 tempText = f.read()
 f.close()
-tempInfo = Text(infoFrame2, height=10, width=25, font=20, yscrollcommand=True)
+tempInfo = Text(infoFrame2, height=5, width=25, font=20, yscrollcommand=True)
 tempInfo.grid(row=0, column=0)
 
 '''
@@ -76,10 +90,24 @@ configButton.grid(row=0, column=0)
 resolutionButton = Button(buttonFrame, text='Resolution', command=resButtonCllbck)
 resolutionButton.grid(row=0, column=1)
 
-# Capture from camera
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+resolutionButton = Button(buttonFrame, text='Close', command=closeButtonCllbck)
+resolutionButton.grid(row=0, column=2)
+
+
+def on_trackbar(val):
+    pass
+
+
+if len(sys.argv)>1 and sys.argv[1]== 'calib':
+    title_window = 'Linear Blend'
+    cv2.namedWindow(title_window)
+    
+    cv2.createTrackbar("Hmax", title_window, 0, 255, on_trackbar)
+    cv2.createTrackbar("Hmin", title_window, 0, 255, on_trackbar)
+    cv2.createTrackbar("Smax", title_window, 0, 255, on_trackbar)
+    cv2.createTrackbar("Smin", title_window, 0, 255, on_trackbar)
+    cv2.createTrackbar("Vmax", title_window, 0, 255, on_trackbar)
+    cv2.createTrackbar("Vmin", title_window, 0, 255, on_trackbar)
 
 def showInfo(gazValue, tempVallue):
     try:
@@ -113,21 +141,130 @@ def showInfo(gazValue, tempVallue):
 
 def readSensor():
     return datetime.now().second, datetime.now().second
+    
+
+def find_biggest_fire(src): # src la anh
+    '''
+    xac dinh mang mau co dien tich lon nhat trong anh trang den 'src'
+    '''
+    _, contours, _ = cv2.findContours(src, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) ## contours la danh sach cac mang mau trang
+    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours] ## lay dien tich cac contour
+    if len(contour_sizes) > 0:
+        biggest_contour = max(contour_sizes, key=lambda x: x[0])[1] ## contour co dien toich lon nhat ## biggest_contour danh sach toa do cac pixel mau trang
+        mask = np.zeros(src.shape, np.uint8)
+        cv2.drawContours(mask, [biggest_contour], -1, 255, -1) ## chuyen tu biggest_contour qua anh mask
+        rect = cv2.boundingRect(biggest_contour)
+        return mask, rect
+    else:
+        return src, [0,0,0,0]
+    
+    
+    
+def thread_camera(threadname):
+    global image
+    cap = PiCamera()
+    cap.resolution = (320, 240)
+    cap.framerate = 15
+    rawCapture = PiRGBArray(cap, size=(320, 240))
+    sleep(0.1)
+    
+    last_white = 0
+    sum_diff = 0
+    loop = 0
+    lower = [0, 0, 165]
+    upper = [26, 37, 255]
+    
+    for frame in cap.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+        
+        # lay gia tri tu trackbar
+        if len(sys.argv)>1 and sys.argv[1]== 'calib':
+            Hmax = cv2.getTrackbarPos('Hmax', title_window)
+            Hmin = cv2.getTrackbarPos('Hmin', title_window)
+            Smax = cv2.getTrackbarPos('Smax', title_window)
+            Smin = cv2.getTrackbarPos('Smin', title_window)
+            Vmax = cv2.getTrackbarPos('Vmax', title_window)
+            Vmin = cv2.getTrackbarPos('Vmin', title_window)
+            # gioi han tren duoi mau lua trong he mau HSV
+            lower = [Hmin, Smin, Vmin]
+            upper = [Hmax, Smax, Vmax]
+        
+        # chuyen doi khong gian mau
+        hsv = cv2.cvtColor(frame.array, cv2.COLOR_BGR2HSV)
+        
+        
+        # chuyen doi lower upper tu dang list sang mang numpy
+        lower_np = np.array(lower, dtype="uint8")
+        upper_np = np.array(upper, dtype="uint8")
+        
+        # lay nhung diem anh co gia tri mau nam trong khoang (lower, upper)
+        mask = cv2.inRange(hsv, lower_np, upper_np)
+        
+        if len(sys.argv)>1 and sys.argv[1]== 'calib':
+            cv2.imshow(title_window, mask)
+            
+        #cv2.imshow("resdfd", mask) ## mask la anh trang den cua cac vung co mau lua
+        mask, rect = find_biggest_fire(mask)
+
+        detected_edges = cv2.Canny(mask, 100, 200, 3) ## lay bien anh
+        
+        # tinh chu vi cua 'detected_edges'
+        n_white = np.sum(detected_edges == 255)
+        
+        # tinh do thay doi chu vi cua 'detected_edges'
+        diff_white = n_white - last_white
+        last_white = n_white
+        
+        # tinh tong do thay doi chu vi qua 30 khung hinh
+        if diff_white > 0 and diff_white < 100:
+            sum_diff += diff_white
+        loop += 1
+
+        if loop > 30:
+            loop = 0
+            if sum_diff > 100:
+                print("fire")
+            else:
+                print("clear")
+            print("fluctuation:", sum_diff)
+            sum_diff = 0
+        # cv2.imshow("cen", detected_edges)
+        # image display and key handling
+
+#        output = cv2.bitwise_and(frame.array, frame.array, mask=mask)
+        
+        # ve khung bao quanh vung 'nghi ngo co lua' lon nhat
+        x, y, w, h = rect
+        cv2.rectangle(frame.array,(x,y),(x+w,y+h),(0,255,0),2)
+        
+        # cv2.imshow(title_window, output)
+        
+        image = frame.array
+        rawCapture.truncate(0)
+        
+        # nhan 'q' de close camera
+        key = cv2.waitKey(1) & 0xFF
+        if end_thread == True:
+            break
 
 
+    
 # function for video streaming
 def video_stream():
     gazValue, tempVallue = readSensor()
     showInfo(gazValue, tempVallue)
-    _, frame = cap.read()
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    
+    cv2image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
     newimage = cv2.resize(cv2image, (resolution[0], resolution[1]))
     img = Image.fromarray(newimage)
     imgtk = ImageTk.PhotoImage(image=img)
     labelVideo.imgtk = imgtk
     labelVideo.configure(image=imgtk)
     labelVideo.after(1, video_stream)
+    
 
+thread1 = Thread(target=thread_camera, args=('Thread-1', ))
+thread1.start()
 
 video_stream()
 root.mainloop()
+
